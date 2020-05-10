11319e0f9439d4256a441f68843102d8dbb4aa77
triptixx <triptixx@gmail.com>
2020-05-10 15:23:27 +0200

    test

--- build.sh
+++ build.sh
@@ -2,5 +2,17 @@
 
 set -exo pipefail
 
-apt-get update
-apt-get -y install bison
+mv configparams /glibc-build/configparams
+
+wget -O- "https://ftp.gnu.org/gnu/glibc/glibc-${GLIBC_VER}.tar.gz" | tar -xz -C /
+
+"/glibc-${GLIBC_VER}/configure" \
+    --prefix="$PREFIX_DIR" \
+    --libdir="${PREFIX_DIR}/lib" \
+    --libexecdir="${PREFIX_DIR}/lib" \
+    --enable-multi-arch \
+    --enable-stack-protector=strong
+make -j$(nproc)
+make install
+
+tar --dereference --hard-dereference -zc -f "/glibc-bin-${GLIBC_VER}.tar.gz" "$PREFIX_DIR"
